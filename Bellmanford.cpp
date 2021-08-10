e6f18c8063e1cc013ef848a30ad9620e1cee68b7
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2021-08-11 01:13:51 +0200

    Update Bellmanford.cpp

--- Bellmanford.cpp
+++ Bellmanford.cpp
@@ -4,9 +4,8 @@ using namespace std;
 vector<int> dis(11);
 vector<pair<int, int>>adj[11];
 
-void bellmanford(int edges) {
-    edges--;
-    while (edges--){
+void bellmanford(int iterations) {
+    while (iterations--){
         for(int i = 0; i < 11; i++){
             for(auto j : adj[i]){
                 dis[j.first] = min(dis[j.first], dis[i]+j.second);
@@ -15,18 +14,18 @@ void bellmanford(int edges) {
     }
 }
 
-void bellmanfordPreCall(int cur, int edges){
+void bellmanfordPreCall(int cur, int nodes){
     for(int i = 0; i < 11; i++)
         dis[i] = INT_MAX;
     dis[cur] = 0;
-    bellmanford(edges);
+    bellmanford(nodes-1);
 }
 
 int main()
 {
 
-    int edges, maxNode;
-    cin >> edges >> maxNode;
+    int edges, nodes, maxNode;
+    cin >> edges >> nodes >> maxNode;
     for (int i = 1; i <= edges; i++){
         int u, v, dis;
         cin >> u >> v >> dis;
@@ -34,7 +33,7 @@ int main()
         //adj[v].push_back({u, dis}); Algorithm don't work with undirected graphs
 
     }
-    bellmanfordPreCall(1, edges); // Shortest path from node 1 to any node
+    bellmanfordPreCall(1, nodes); // Shortest path from node 1 to any node
     for(int i = 1; i <= maxNode; i++){
         cout << dis[i] << " ";
     }
